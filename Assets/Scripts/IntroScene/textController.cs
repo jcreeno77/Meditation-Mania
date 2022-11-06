65bcbc625a001583539b8d6776b046b721deee0c
jcreeno77 <jcreeno77@gmail.com>
2022-11-05 21:08:03 -0500

    Intro Scene added and refined

--- Assets/Scripts/IntroScene/textController.cs
+++ Assets/Scripts/IntroScene/textController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class textController : MonoBehaviour
 {
@@ -12,11 +13,18 @@ public class textController : MonoBehaviour
     [SerializeField] GameObject text3;
     [SerializeField] GameObject text4;
     [SerializeField] GameObject playBtn;
-    [SerializeField] Scene nextScene;
+    [SerializeField] GameObject vidRenderer;
+    [SerializeField] GameObject vidPlayer;
+    [SerializeField] AudioSource audsrc;
     float timer;
     float color1;
     float color2;
     float color3;
+    float alphaVideo;
+    float alphaBtn;
+    bool audioPlayed;
+
+    Button BeginGame;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +32,13 @@ public class textController : MonoBehaviour
         color1 = 0f;
         color2 = 0f;
         color3 = 0f;
+        alphaVideo = 0f;
+        alphaBtn = 0f;
+        audsrc = vidPlayer.GetComponent<AudioSource>();
+        audioPlayed = false;
+
+        BeginGame = playBtn.GetComponent<Button>();
+        BeginGame.onClick.AddListener(StartGame);
     }
 
     // Update is called once per frame
@@ -54,6 +69,20 @@ public class textController : MonoBehaviour
         if (timer > 21)
         {
             text4.GetComponent<Text>().color = new Color(1, 1, 1, 1);
+            if (!audioPlayed)
+            {
+                audsrc.Play();
+                audioPlayed = true;
+            }
+        }
+
+        
+
+        if (timer > 23)
+        {
+            alphaVideo += Time.deltaTime;
+            vidPlayer.GetComponent<VideoPlayer>().Play();
+            alphaBtn += Time.deltaTime;
         }
 
         color1 = Mathf.Clamp(color1, 0, 1);
@@ -62,5 +91,14 @@ public class textController : MonoBehaviour
         text1.GetComponent<Text>().color = new Color(1, 1, 1, color1);
         text2.GetComponent<Text>().color = new Color(1, 1, 1, color2);
         text3.GetComponent<Text>().color = new Color(1, 1, 1, color3);
+        vidRenderer.GetComponent<RawImage>().color = new Color(1, 1, 1, alphaVideo);
+        playBtn.GetComponent<Image>().color = new Color(1, 1, 1, alphaBtn);
+        playBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(0, 0, 0, alphaBtn);
+    }
+
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
